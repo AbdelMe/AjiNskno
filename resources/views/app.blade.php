4161b91c97cd0c58ba6e6565a84058fa29ec737c
AbdelMe <simoelabdellaoui08@gmail.com>
2025-05-06 10:57:37 +0100

    update app.css & vite config to use tailwindCss

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @vite('resources/js/app.js')
+    <title>AjiNskno | React Laravel App</title>
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
     @inertiaHead
 </head>
 <body>
