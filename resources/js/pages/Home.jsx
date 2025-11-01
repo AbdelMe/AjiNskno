ceff5049ba5a215027c4fd1b5b98c9d69a96e990
AbdelMe <simoelabdellaoui08@gmail.com>
2025-11-01 23:01:43 +0000

    add migration for products table with price and quantity fields; update routes for product API

--- resources/js/pages/Home.jsx
+++ resources/js/pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <>
-            <h1 className="bg-indigo-600 text-center py-8 text-4xl">hello I'm Using react/Inertia</h1>
+            <h1 className="bg-indigo-200 text-center py-8 text-4xl">hello I'm  react/Inertia</h1>
         </>
     );
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/Home', function () {
     return inertia('Home');
 });
+
+// Route::get('/api/test', function () {
+//     return 'API works!';
+// });
+
+// Route::get('/api/products', [ProductController::class, 'index']);  // list products
+// Route::post('/api/products', [ProductController::class, 'store']); // add product
