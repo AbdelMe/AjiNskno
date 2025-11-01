05873818610834038eb1c8cf93434159b4272ebf
AbdelMe <simoelabdellaoui08@gmail.com>
2025-11-01 23:00:44 +0000

    add api routing to application configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,10 +8,11 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',   // <-- add this line
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function ($middleware) {
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
